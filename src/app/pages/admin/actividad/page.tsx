abae7177cf5f90d128200fc2d106fd00fce59357
EDUARDJ1001 <123607036+EDUARDJ1001@users.noreply.github.com>
2025-01-15 08:57:02 -0600

    ADD: Visualizar actividad por Vias

--- src/app/pages/admin/actividad/page.tsx
+++ src/app/pages/admin/actividad/page.tsx
@@ -9,30 +9,30 @@ const Actividad = () => {
       <div className="flex flex-1">
         <SidebarAdmin />
         <div className="container mx-auto p-6">
-          <h1 className="text-3xl font-bold text-gray-800 mb-4">Gestion de actividad por vias</h1>
-          <p className="text-lg text-gray-600 mb-6">Aquí podrá gestionar la actividad de la via que este utilizando este sistema.</p>
+          <h1 className="text-3xl font-bold text-gray-800 mb-4">Vista de actividad por vias</h1>
+          <p className="text-lg text-gray-600 mb-6">Aquí podrá visualizar la actividad de la via que este utilizando este sistema.</p>
 
           <div className="flex flex-wrap gap-4">
             <a
-              href="/pages/admin/tickets"
+              href="/pages/admin/actividad/via1"
               className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
             >
               Via 1
             </a>
             <a
-              href="/pages/admin/tickets"
+              href="/pages/admin/actividad/via2"
               className="bg-orange-600 hover:bg-orange-700 text-white font-bold py-3 px-6 rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
             >
               Via 2
             </a>
             <a
-              href="/pages/admin/tickets"
+              href="/pages/admin/actividad/via3"
               className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
             >
               Via 3
             </a>
             <a
-              href="/pages/admin/tickets"
+              href="/pages/admin/actividad/via4"
               className="bg-orange-600 hover:bg-orange-700 text-white font-bold py-3 px-6 rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
             >
               Via 4
---
+++ src/app/pages/admin/actividad/via2/page.tsx
@@ -0,0 +1,138 @@
+"use client";
+
+import HeaderAdmin from "@/app/components/headerAdmin";
+import SidebarAdmin from "@/app/components/sideBar";
+import React, { useState, useEffect } from "react";
+
+interface User {
+    nombre: string;
+    apellido: string;
+    loginTime: string;
+}
+
+interface ConteoBoleto {
+    Descripcion: string;
+    Valor: number;
+    Cantidad: number;
+    Total?: number;
+}
+
+const ActividadVia2: React.FC = () => {
+    const [userData, setUserData] = useState<User | null>(null);
+    const [conteoBoletos, setConteoBoletos] = useState<ConteoBoleto[]>([]);
+    const totalConteoBoletos = conteoBoletos.reduce(
+        (acc, boleto) => acc + boleto.Valor * boleto.Cantidad,
+        0
+    );
+    const apiHost = process.env.NEXT_PUBLIC_API_HOST || "";
+
+    useEffect(() => {
+        fetch(`${apiHost}/api/conteo-boletos-v2`)
+            .then((res) => res.json())
+            .then((data) => {
+                setConteoBoletos(data);
+            })
+            .catch((err) => console.error("Error al cargar conteo de boletos:", err));
+
+        const storedUser = localStorage.getItem("user");
+        if (storedUser) {
+            const parsedUser = JSON.parse(storedUser);
+            setUserData({
+                ...parsedUser,
+                loginTime: new Date(parsedUser.loginTime).toLocaleString(),
+            });
+        }
+    }, [apiHost]);
+
+    return (
+        <div className="bg-gray-100 min-h-screen flex flex-col">
+            <HeaderAdmin />
+            <div className="flex flex-1">
+                <SidebarAdmin/>
+                <div className="container mx-auto p-6">
+                    <h1 className="text-3xl font-bold text-gray-800 mb-4">Actividad de Via #2</h1>
+                    <div className="p-6 space-y-8">
+                        <div id="report-content">
+                            <div className="flex justify-between items-center">
+                                <img
+                                    src="/img/municipalidad.png"
+                                    alt="Logo Municipalidad"
+                                    className="h-14 w-14"
+                                />
+                                <img
+                                    src="/img/PEAJE.png"
+                                    alt="Logo peaje"
+                                    className="h-14 w-16"
+                                />
+                            </div>
+                            <div className="mb-4 text-center">
+                                <h2 className="text-xl font-bold">ACTIVIDAD VIA #2</h2>
+                                <p className="text-lg">DEPARTAMENTO DE PEAJE</p>
+                                <p className="text-lg">MUNICIPIO DE PUERTO CORTÉS</p>
+                                <p className="text-lg">FECHA: {new Date().toLocaleDateString()}</p>
+                                <div className="flex justify-between mt-2">
+                                    <div>
+                                        <p>VIA #2</p>
+                                    </div>
+                                    <div>
+                                        <p>
+                                            OPERADOR: {userData?.nombre} {userData?.apellido}
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="flex justify-between mt-2">
+                                    <div>
+                                        <p>INICIO DE SESION: {userData?.loginTime}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <h2 className="text-lg font-bold mb-4">Conteo de Boletos</h2>
+                                <table className="table-auto w-full border-collapse border border-gray-300">
+                                    <thead className="bg-gray-100">
+                                        <tr>
+                                            <th className="border border-gray-300 p-2">Tipo Boleto</th>
+                                            <th className="border border-gray-300 p-2">Valor Boleto</th>
+                                            <th className="border border-gray-300 p-2">Cantidad</th>
+                                            <th className="border border-gray-300 p-2">Total</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {conteoBoletos.length > 0 ? (
+                                            conteoBoletos.map((boleto, index) => (
+                                                <tr key={index} className="even:bg-gray-50">
+                                                    <td className="border border-gray-300 p-2">{boleto.Descripcion}</td>
+                                                    <td className="border border-gray-300 p-2">{boleto.Valor}</td>
+                                                    <td className="border border-gray-300 p-2">{boleto.Cantidad}</td>
+                                                    <td className="border border-gray-300 p-2">{boleto.Total || '0.00'}</td>
+                                                </tr>
+                                            ))
+                                        ) : (
+                                            <tr>
+                                                <td colSpan={4} className="border border-gray-300 p-2 text-center">
+                                                    No hay datos disponibles
+                                                </td>
+                                            </tr>
+                                        )}
+                                    </tbody>
+                                    <tfoot>
+                                        <tr className="bg-gray-100">
+                                            <td colSpan={3} className="border border-gray-300 p-2 font-bold text-right">
+                                                Total en Boletos
+                                            </td>
+                                            <td className="border border-gray-300 p-2 font-bold">
+                                                {totalConteoBoletos.toFixed(2)}
+                                            </td>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ActividadVia2;
