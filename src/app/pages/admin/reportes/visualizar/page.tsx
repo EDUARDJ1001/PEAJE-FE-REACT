7f2696721bd66ea99dda08816e004f54e9737771
EDUARDJ1001 <123607036+EDUARDJ1001@users.noreply.github.com>
2025-01-20 09:20:02 -0600

    Add: Sidebar visualizar reportes

--- src/app/pages/admin/reportes/visualizar/page.tsx
+++ src/app/pages/admin/reportes/visualizar/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import HeaderAdmin from "@/app/components/headerAdmin";
+import SidebarAdmin from "@/app/components/sideBar";
 import React, { useState, useEffect } from "react";
 
 const Reportes: React.FC = () => {
     return (
-        <div>
+        <div className="bg-gray-100 min-h-screen flex flex-col">
             <HeaderAdmin />
-            <div>
-                <h1 className="text-3xl font-bold text-gray-800 mb-4">Reportes Existentes</h1>
+            <div className="flex flex-1">
+                <SidebarAdmin />
+                <div>
+                    <h1 className="text-3xl font-bold text-gray-800 mb-4">Reportes Existentes</h1>
+                </div>
             </div>
         </div>
     );
